764376bddeb76fadb4e55d97b708e4f798d8318b
Mgepahmge <yss489589139@outlook.com>
2024-11-14 20:29:21 +0800

    Fix CMakeLists.txt

--- include/NeuZephyr/dl_export.cuh
+++ include/NeuZephyr/dl_export.cuh
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef _MSC_VER
-    #ifdef DL_FRAMEWORK_EXPORTS
+    #ifdef NEUZEPHYR_EXPORTS
         #define DL_API __declspec(dllexport)
     #else
         #define DL_API __declspec(dllimport)
