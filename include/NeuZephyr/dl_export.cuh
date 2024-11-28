d77de32d975a0ca0af76854e0eb9e150f3604aa5
Mgepahmge <yss489589139@outlook.com>
2024-11-28 14:38:00 +0800

    refactor(build): update CMakeLists.txt

--- include/NeuZephyr/dl_export.cuh
+++ include/NeuZephyr/dl_export.cuh
@@ -1,11 +1,14 @@
 #pragma once
 
-#ifdef _MSC_VER
+// Cross-platform export macro
+#if defined(_WIN32) || defined(_WIN64)
     #ifdef NEUZEPHYR_EXPORTS
         #define DL_API __declspec(dllexport)
     #else
         #define DL_API __declspec(dllimport)
     #endif
+#elif defined(__GNUC__) && __GNUC__ >= 4
+    #define DL_API __attribute__((visibility("default")))
 #else
     #define DL_API
 #endif
