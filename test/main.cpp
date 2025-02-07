e8c279c9269ebd9cab46debef0a2129e7ca3e142
Mgepahmge <yss489589139@outlook.com>
2025-02-07 17:24:12 +0800

    update doc

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,7 @@ using namespace nz;
 
 
 int main() {
+    /*
     data::Tensor a1({9, 1}, true);
     data::Tensor real({9, 1}, true);
     float a1_data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -12,7 +13,7 @@ int main() {
     nodes::io::InputNode input1(a1);
     nodes::io::InputNode input2(real);
     nodes::calc::SoftmaxNode math(&input1);
-    nodes::loss::BinaryCrossEntropyNode output(&math, &input2);
+    nodes::loss::MeanSquaredErrorNode output(&math, &input2);
     math.forward();
     output.forward();
     std::cout << output << std::endl;
@@ -22,5 +23,19 @@ int main() {
     opt::AdaDelta optimizer(0.01);
     optimizer.step(&input1);
     std::cout << input1 << std::endl;
+    */
+    data::Tensor A({3, 4}, true);
+    data::Tensor B({4, 3}, true);
+    A.fill(1);
+    B.fill(1);
+    nodes::io::InputNode input1(A);
+    nodes::io::InputNode input2(B);
+    nodes::calc::MatMulNode math(&input1, &input2);
+    math.forward();
+    std::cout << math << std::endl;
+    math.output->fillGrad(1);
+    math.backward();
+    std::cout << input1 << std::endl;
+    std::cout << input2 << std::endl;
     return 0;
 }
